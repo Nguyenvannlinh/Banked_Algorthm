f8751e61f151be6cb04c3433c3c543955c086972
Nguyễn Văn linh <127535814+Nguyenvannlinh@users.noreply.github.com>
2023-10-25 10:58:31 +0700

    Update Program.cs

--- Banked_Algorthm/Program.cs
+++ Banked_Algorthm/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Text;
 
@@ -6,8 +6,10 @@ namespace banked_alogorthm
 {
     class program
     {
-
-        static int[,] need(int n, int m, int[,] y, int[,] z)
+        public int m, n;
+        public int[,] allo, max, need, request;
+        public int[] avil;
+        static int[,] Need(int n, int m, int[,] y, int[,] z)
         {
             int[,] x = new int[n, m];
             for (int i = 0; i < n; i++)
@@ -72,25 +74,52 @@ namespace banked_alogorthm
             }
             return safa;
         }
-        static bool check(int  n)
+                                                // avail  allo     need       request
+        static int[] Request(int n, int x, int m, int[]b, int[,]y , int[,] z, int[,] a)
+        {
+            bool[] test = new bool[m];
+            int check = 0;
+            for (int j = 0; j < m; j++)
+            {
+                if (a[x, j] <= b[j])
+                {
+                    test[j] = true;
+                    check++;
+                }
+            }
+            if (check == m)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    y[x, j] += a[x, j];
+                    z[x, j] -= a[x, j];
+                    b[j] -= a[x, j];
+
+                }
+            }
+            int [] nao = Safa(n, m, z, y, b);
+            return nao;
+            
+        }
+        static bool Check(int  n)
         {
             return n < 0;
         }
-        static int[] input()
+        static void input()
         {
             int m,n;
             while (true)
             {
                 Console.Write("Số lượng tiến trình : ");
                  n = int.Parse(Console.ReadLine());
-                if (!check(n)) { break; }
+                if (!Check(n)) { break; }
             }
             while (true)
             {
                 
                 Console.Write("Số lượng tài nguyên : ");
                 m = int.Parse(Console.ReadLine());
-                if (!check(m)) { break; }
+                if (!Check(m)) { break; }
             }
             int[] avil = new int[n];
             for (int i = 0;i < m; i++)
@@ -99,21 +128,22 @@ namespace banked_alogorthm
                 {
                     Console.Write($"Tài nguyên mỗi loại R{i} :");
                     avil[i] = int.Parse(Console.ReadLine());
-                    if (!check(avil[i])) { break; }
+                    if (!Check(avil[i])) { break; }
                 }
             }
             Console.Clear();
             int[,] max = new int[n,m];
             for (int i = 0; i < n; i++)
             {
-                for(int j = 0;j < m; j++)
+                Console.WriteLine($"Tiến trình P{i}: ");
+                for (int j = 0;j < m; j++)
                 {
-                    Console.WriteLine($"Tiến trình P{i}: ");
+                    
                     while (true)
                     {
                         Console.Write($"Tài nguyên tối đa của tiến trình[{i}, {j}] :");
                         max[i, j] = int.Parse(Console.ReadLine());
-                        if ((max[i,j] <= avil[j]) && !check(max[i, j])) { break; }
+                        if ((max[i,j] <= avil[j]) && !Check(max[i, j])) { break; }
                     }
                 }
             }
@@ -121,31 +151,58 @@ namespace banked_alogorthm
             int[,] allo = new int[n, m];
             for (int i = 0; i < n; i++)
             {
+                Console.WriteLine($"Tiến trình P{i}: ");
                 for (int j = 0; j < m; j++)
                 {
-                    Console.WriteLine($"Tiến trình P{i}: ");
+                    
                     while (true)
                     {
                         Console.Write($"Tài nguyên hiện có của tiến trình[{i}, {j}] :");
                         allo[i, j] = int.Parse(Console.ReadLine());
-                        if ((allo[i, j] <= max[i, j]) && !check(allo[i, j])) { break; }
+                        if ((allo[i, j] <= max[i, j]) && !Check(allo[i, j])) { break; }
                     }
                 }
             }
-            if(Safa(n, m, need(n, m, max, allo), allo, available(n, m, allo, avil)).Length == n)
+            int[,] request = new int[n, m];
+            if(Safa(n, m, Need(n, m, max, allo), allo, available(n, m, allo, avil)).Length == n)
             {
-                return Safa(n, m, need(n, m, max, allo), allo, available(n, m, allo, avil));
+                Console.WriteLine($"Hệ thông an toàn\nDãy an toàn là:{Hethong(Safa(n, m, Need(n, m, max, allo), allo, available(n, m, allo, avil)))}");
+                Console.WriteLine("\nnhấn enter để request tài nguyên");
+                Console.ReadKey();
+                int x;
+                Console.Clear();
+                while (true)
+                {
+                    Console.Write("Nhập tiến trình  có yêu cầu tài nguyên: ");
+                    x = int.Parse(Console.ReadLine());
+                    if (!Check(x) && x < n) { break; }
+                }
+                for (int j = 0; j < m; j++)
+                {
+
+                    Console.WriteLine($"Tiến trình P{j}: ");
+                    while (true)
+                    {
+                        Console.Write($"Tài nguyên tối đa của tiến trình[{x}, {j}] :");
+                        request[0, j] = int.Parse(Console.ReadLine());
+                        if ((request[x, j] <= Need(n, m, max, allo)[x, j]) && !Check(request[x, j])) { break; }
+                    }
+
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"Hệ thông được làm mới\nDãy an toàn là: {Hethong(Request(n, x, m, available(n, m, allo, avil), allo, Need(n, m, max, allo), request))}");
             }
-            return new int[] {};
-           
+            else { Console.WriteLine("Hệ thông không an toàn"); }
+            
+
         }
-        static void Hethong(int[] x)
+        
+        static string Hethong(int[] x)
         {
             Console.Clear();
             if (x.Length != 0)
             {
-                Console.WriteLine("hệ thống an toàn");
-                Console.Write("dãy an toàn là :");
                 string a = "";
                 for (int i = 0; i < x.Length; i++)
                 {
@@ -155,15 +212,14 @@ namespace banked_alogorthm
                 {
                     a = a.Substring(0, a.Length - 4);
                 }
-                Console.WriteLine($"{a}");
+                return a;
             }
-            else { Console.WriteLine("hệ thông đang ở trạng tháy không an toàn "); }
+            else { return " "; }
         }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
-            int[] a = input();
-            Hethong(a);
+            input();
         }
     }
 }
